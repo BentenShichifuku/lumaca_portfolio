278b5dbea52fedb45e460be0e9392114b3b9a15f
BentenShichifuku <yoshida.yuto.23@shizuoka.ac.jp>
2025-05-05 03:30:26 +0900

    初期完成！BraveBirdローンチ版

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -4,16 +4,16 @@ import Image from "next/image";
 export default function ProfileCard() {
     return (
         <section className="flex justify-center py-20 bg-white/70 backdrop-blur-md">
-        <div className="bg-white rounded-xl shadow-lg p-8 max-w-2xl w-full text-gray-800">
-        <div className="flex flex-col items-center text-center space-y-4">
+        <div className="bg-white rounded-xl shadow-lg p-14 max-w-6xl w-full text-gray-800">
+        <div className="flex flex-col items-center text-center space-y-8">
             <Image
                 src="/profile.jpg"
                 alt="プロフィール写真"
-                width={120}
-                height={120}
+                width={180}
+                height={180}
                 className="rounded-full shadow-md"
             />
-            <h2 className="text-2xl font-bold">BraveBird</h2>
+            <h2 className="text-4xl font-bold">BraveBird</h2>
             <p className="text-sm">所属：静岡大学 情報学部 狩野研究室</p>
             <p className="text-sm">Mail：yyoshida@kanolab.next</p>
             <p className="text-sm">その他：XXX</p>
